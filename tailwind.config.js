74917827c8b5e0d9469e2b8ffce7bc815d2a93e8
Susmita Biswas <145971011+SusmitaBiswasAdor@users.noreply.github.com>
2024-12-16 07:58:33 +1200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  
+  plugins: [],
+};
+
+
